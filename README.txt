380119fa5821ac576bd7a96e02c8af79c010c19d
Itamar Tzadok <itamar@substantialmethods.com>
2012-09-30 15:03:36 -0400

    Initial commit.

---
+++ README.txt
@@ -0,0 +1,3 @@
+moodle-local_messageprovider
+=============================
+A local plugin that sends new self-registration notifications to admin.
---
+++ locallib.php
@@ -0,0 +1,74 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    local
+ * @subpackage messageprovider
+ * @copyright  2012 Itamar Tzadok
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') or die;
+
+/**
+ * Event handler for self registration user created event
+ */
+class self_registration_handler {
+    public static function notify_admin($user) {
+        global $CFG, $SITE;
+
+        // Skip if not auth email
+        if (empty($user->auth) or $user->auth !== 'email') {
+            return true;
+        }
+        
+		// Prepare message
+        $userfullname = fullname($user);
+        $userprofilelink = html_writer::link(new moodle_url('/user/view.php', array('id' => $user->id)), $userfullname);
+
+		$sitename = format_string($SITE->fullname);
+        $subjectdata = (object) array(
+            'sitename' => $sitename,
+            'userfullname' => $userfullname
+        );
+		$subject = get_string('subject', 'local_messageprovider', $subjectdata);
+        $user->userprofilelink = $userprofilelink;
+        $user->userfullname = $userfullname;
+		$content = get_string('content', 'local_messageprovider', $user);
+		$contenthtml = text_to_html($content, false, false, true);
+	    $admin = get_admin();
+		
+        // Send message
+        $message = new object;
+        $message->siteshortname   = format_string($SITE->shortname);
+        $message->component       = 'local_messageprovider';
+        $message->name            = 'newselfregistration';
+        $message->userfrom        = $admin;
+        $message->userto          = $admin;
+        $message->subject         = $subject;
+        $message->fullmessage     = $content;
+        $message->fullmessageformat = FORMAT_HTML;
+        $message->fullmessagehtml = $contenthtml;
+        $message->smallmessage    = '';
+        $message->notification    = 1;
+
+        message_send($message);
+
+        return true;
+    }
+
+}
